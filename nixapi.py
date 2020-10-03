080b6c4242884a935a4ef34d028425e1979001f5
James Brown <james@catch22.net>
2020-10-03 19:42:13 +0100

    Nixplay / Flickr album sync first commit

---
+++ nixapi.py
@@ -0,0 +1,173 @@
+import requests
+from requests_toolbelt.utils import dump
+import json
+import jsons
+from datetime import datetime
+
+class NixPlay(object):
+  def __init__(self):
+    self.crsftok = None
+    self.cookies = None
+    self.session = requests.Session()
+
+  def headers(self):
+    return { 
+      'X-CSRF-Token': self.crsftok,
+      'X-Nixplay-Username': self.user, 
+      'X-Requested-With': 'XMLHttpRequest',
+      'Referer': 'https://app.nixplay.com/',
+      'Origin': 'https://app.nixplay.com',
+      'Sec-Fetch-Dest': 'empty',
+      'Sec-Fetch-Mode': 'cors',
+      'Sec-Fetch-Site': 'same-site',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
+    }       
+
+  def login(self, user, password):
+    self.user = f'{user}@mynixplay.com'
+
+    data = {
+      'email': user,
+      'password': password,
+      'signup_pair': 'no_pair',
+      'login_remember': 'false'
+    }
+    hdr = {
+      'Referer': 'https://app.nixplay.com/login',
+      'Origin': 'https://app.nixplay.com',
+      'Sec-Fetch-Dest': 'empty',
+      'Sec-Fetch-Mode': 'cors',
+      'Sec-Fetch-Site': 'same-site'
+    }
+    r = self.session.post('https://api.nixplay.com/www-login/', headers=hdr,data=data)
+    #data = dump.dump_all(r)
+    #print(data.decode('utf-8'))
+
+    j = json.loads(r.text)
+    token = j['token']
+    
+    data = {
+      'token': token,
+      'startPairing': 'false',
+      'redirectPath': ''
+    }
+    hdr = {
+      'Referer': 'https://app.nixplay.com/login',
+      'Origin': 'https://app.nixplay.com',
+      'Sec-Fetch-Dest': 'document',
+      'Sec-Fetch-Mode': 'navigate',
+      'Sec-Fetch-Site': 'same-site',
+      'Sec-Fetch-User': '?1',
+      'Upgrade-Insecure-Requests': '1'
+    }
+    r = self.session.post('https://api.nixplay.com/v2/www-login-redirect/', headers=hdr,data=data, allow_redirects=False)    
+    #data = dump.dump_all(r)
+    #print(data.decode('utf-8'))
+
+    self.cookies = r.cookies
+    #j - json.loads(r.text)
+
+    # save the CSRF token
+    self.csrftok  = r.cookies.get('prod.csrftoken')
+    self.flickr_auth = r.cookies.get('prod.flickr.access_token')
+    
+    return j
+
+
+  def flickr_api(self, method, params={}):
+    u = f'https://api.nixplay.com/v2/social_api/flickr/services/rest'
+    defparams = {
+      'api_key':'3a95aeba90bc3698ba73fb076f7ed370',
+      'auth_token':self.flickr_auth,
+      'format': 'json',
+      'method':method,
+      'nojsoncallback':1,
+      #'page':1,
+      'per_page':100,
+      #'primary_photo_extras':''
+      #'url_m,url_o
+    }
+    #print(Fore.GREEN+Style.BRIGHT+f'\n{u} <{method}> {json.dumps(params)}'+Style.RESET_ALL)
+    #print(self.headers())
+
+    r = self.session.get(u, headers = self.headers(), params={**defparams,**params})#,cookies=self.cookies)
+    #print(r.request.url)
+    #print(r.request.headers)
+    #print(r.text)
+    #print(r.request.url)
+    #data = dump.dump_all(r)
+    #print(data.decode('utf-8'))
+    #?api_key={self.apikey}&auth_token={self.auth_token}&format=json&method={method}&nojsoncallback=1'
+    return json.loads(r.text)
+
+  def get_api_v3(self, method, params={}):
+    u = f'https://api.nixplay.com/v3/{method}'
+    defparams={}#**defparams,**params}
+    #print(Fore.GREEN+Style.BRIGHT+'\n' + u + Style.RESET_ALL)
+    r = self.session.get(u, headers = self.headers(), params={**defparams,**params})
+    return json.loads(r.text)
+
+  def post_api_v3(self, method, params={}, data={}):
+    u = f'https://api.nixplay.com/v3/{method}'
+    print('\n'+u)
+    r = self.session.post(u, headers = self.headers(), params={**defparams,**params}, data=data)
+    return json.loads(r.text)    
+
+  #
+  # NixPlay API
+  #
+
+  def getOnlineStatus(self):
+    return self.get_api_v3('frame/online-status/')
+
+  def getFrames(self):
+    return self.get_api_v3('frames')
+
+  def getPlayLists(self):
+    return self.get_api_v3('playlists')
+
+  def getPlayList(self, name):
+    playlists = self.getPlayLists()
+
+    for p in playlists:
+      if p['name'] == name:
+        return p
+
+  def getPlayListSlides(self, playlist_id, offset=0, size=100):
+    return self.get_api_v3(f'playlists/{playlist_id}/slides', {'size': size, 'offset': offset})
+
+  def addPlayListPhotos(self, playlist_id, photos):
+    return self.post_api_v3(f'playlists/{playlist_id}/items', photos)
+
+  #
+  # Social / Flickr API
+  #
+
+  def flickr_people_getPhotos(self, page=1):
+    return self.flickr_api('flickr.people.getPhotos', {'photoset_id': photoset_id, 'page': page, 'per_page':1, 'user_id':'me', 'extras':'url_m,url_o'})
+   
+  def flickr_photosets_getPhotos(self, photoset_id,page=1):
+    return self.flickr_api('flickr.photosets.getPhotos', {'photoset_id': photoset_id, 'page': page, 'per_page':100, 'extras':'url_m,url_k,url_o,date_upload,last_update'})
+
+  def flickr_photosets_getList(self, page=1):
+    #primary_photo_extras=url_m,url_o
+    return self.flickr_api('flickr.photosets.getList', {'page': page, 'per_page': 100})
+
+  def flickr_photosets_getWithName(self, name):
+    photosets = self.flickr_photosets_getList()
+    
+    for photoset in photosets['photosets']['photoset']:
+      #print(photoset['id'], photoset['title']['_content'])
+      if photoset['title']['_content'] == name:
+        return photoset
+        #photoset_id = photoset['id']   
+
+  def flickr_photosets_getInfo(self, photoset_id):
+    return self.flickr_api('flickr.photosets.getInfo', {'photoset_id': photoset_id})
+
+  def flickr_urls_getUserProfile(self):
+    return self.flickr_api('flickr.urls.getUserProfile')
+
+  def flickr_favorites_getList(self, page=1):
+    #primary_photo_extras=url_m,url_o
+    return self.flickr_api('flickr.favorites.getList', {'page': page, 'per_page': 100})
---
+++ nixflix.py
@@ -0,0 +1,182 @@
+import argparse
+import json
+import jsons
+from datetime import datetime
+import dateutil.parser
+import os
+import sys
+
+from nixapi import NixPlay
+from colorama import Fore, Back, Style, init
+try:
+    init()
+except:
+    print('No colour support')
+
+def demo(args):
+
+  np = NixPlay()
+  np.login(args.username, args.password)
+
+  # Frames
+  frames = np.getFrames()
+  #print(json.dumps(frames, indent=2))  
+  for f in frames:
+    print(f['id'], f['name'])
+
+  # Playlists
+  playlists = np.getPlayLists()
+  #print(json.dumps(playlists, indent=2))
+
+  for p in playlists:
+    print(p['id'], p['name'], f"({p['picture_count']})", p['last_updated_date'])#datetime.fromtimestamp(p['lastUpdate']/1000))  
+
+    if p['name'] == 'My Playlist':
+      #print(Color.Red+p[''])
+      playlist_id = p['id']
+
+  # Online status
+  status = np.getOnlineStatus()
+  print(json.dumps(status, indent=2))
+  for f in status['frames']:
+    ls = datetime.fromtimestamp(int(f['lastConnected'])/100)
+    print(f'lastSeen: {ls}')
+
+  # Slides
+  slides = np.getPlayListSlides(playlist_id)
+  print(json.dumps(slides, indent=2))
+  for slide in slides['slides']:
+    ts = datetime.fromtimestamp(slide['timestamp']/1000)
+    print(slide['playlistItemId'], ts)
+    pass
+
+  # user profile
+  print('')
+  profile = np.flickr_urls_getUserProfile()
+  print(json.dumps(profile, indent=2))
+
+  # Flickr albums
+  photolist = np.flickr_photosets_getList()
+  #print(json.dumps(photolist, indent=2))
+
+  for photoset in photolist['photosets']['photoset']:
+    print(photoset['id'], photoset['title']['_content'])
+    if photoset['title']['_content'] == 'Favs':
+      photoset_id = photoset['id']   
+      
+  # Album info
+  info = np.flickr_photosets_getInfo(photoset_id)
+  print(json.dumps(info, indent=2))
+
+  # Album photos
+  photos = np.flickr_photosets_getPhotos(photoset_id)
+  print(json.dumps(photos, indent=2))
+
+  for photo in photos['photoset']['photo']:
+    lu = datetime.fromtimestamp(int(photo["lastupdate"]))
+    du = datetime.fromtimestamp(int(photo["dateupload"]))
+    print(f'{photo["id"]} \"{photo["title"]}\" - {lu} - {du} ')#{photo["url_o"]}')
+
+
+  items = {
+    "items": [
+      { 'orientation': 1 },
+      { 'photoUrl': 'https://live.staticflickr.com/.....' },
+      { 'thumbnailUrl': 'https://live.staticflickr.com/.....' }
+    ]
+  }
+
+  #{"items":[
+  #{"photoUrl":"https://live.staticflickr.com/65535/50382815533_de06b99634_k.jpg","thumbnailUrl":"https://live.staticflickr.com/65535/50382815533_0fcd8ceb05.jpg","orientation":1},
+  #{"photoUrl":"https://live.staticflickr.com/65535/50398382646_e5fa29725f_k.jpg","thumbnailUrl":"https://live.staticflickr.com/65535/50398382646_621fa40f1d.jpg","orientation":1},
+  #{"photoUrl":"https://live.staticflickr.com/65535/50405265737_d48fad2c00_k.jpg","thumbnailUrl":"https://live.staticflickr.com/65535/50405265737_12844d3e9c.jpg","orientation":1}
+  #]}
+
+  #np.addPlayListPhotos(playlist_id, items)
+
+  #POST api.nixplay.com/v3/playlists/3201700/items
+  #{items:[
+  #orientation: 1
+  #photoUrl: "https://live.staticflickr.com/31337/49849262952_82684bd73e_o.jpg"
+  #thumbnailUrl: "https://live.staticflickr.com/31337/49849262952_49382bb194.jpg"
+  #]}
+  #
+  #
+  ##
+
+  # flickr:
+  #method=flickr.photosets.getList&page=1&per_page=30&primary_photo_extras=url_m,url_o
+  #method=flickr.urls.getUserProfile
+  #method=flickr.photosets.getInfo&nojsoncallback=1&photoset_id=72157713990481416
+  #method=flickr.photosets.getPhotos&nojsoncallback=1&page=1&per_page=30&photoset_id=72157713990481416
+
+
+
+  favs = np.flickr_favorites_getList(photoset_id)
+  print(json.dumps(favs, indent=2))
+
+
+def main(args):
+  np = NixPlay()
+  np.login(args.username, args.password)
+
+  # Nixplay playlist
+  playlist = np.getPlayList(args.playlist)  
+  if not playlist:
+    print(f'Playlist not found: {args.playlist}')
+    return 1
+  #print(json.dumps(playlist, indent=2))
+
+  np_last_updated = dateutil.parser.isoparse(playlist['last_updated_date'])
+  print(f'Nixplay last updated: {np_last_updated}')
+
+  # Flickr album
+  photoset = np.flickr_photosets_getWithName(args.album)
+  print(json.dumps(photoset, indent=2))
+  flickr_last_updated = datetime.fromtimestamp(int(photoset['date_update']))
+  print(f'Flickr album updated: {flickr_last_updated}')
+
+
+  # get list of flickr photots in album
+  photos = np.flickr_photosets_getPhotos(photoset['id'])
+  #print(json.dumps(photos, indent=2))
+
+  items = { "items": [] }
+
+  for photo in photos['photoset']['photo']:
+    updated = datetime.fromtimestamp(int(photo["lastupdate"]))
+    
+    item = { 
+      "photoUrl":     photo["url_o"], 
+      "thumbnailUrl": photo["url_m"],
+      "orientation":  1
+    }
+      
+    items['items'].append(item)
+  
+  #print(items) 
+  np.addPlayListPhotos(playlist['id'], items)
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser('Nixplay / Flickr album sync')
+  parser.add_argument('--username', help='Nixplay username')
+  parser.add_argument('--password', help='Nixplay password')
+  parser.add_argument('--nixplay-list', dest = 'playlist', default='My Playlist')
+  parser.add_argument('--flickr-album', dest = 'album',    default='Favs')
+
+  args = parser.parse_args()
+  if not args.username and 'NIXPLAY_USERNAME' in os.environ:
+    args.username = os.environ['NIXPLAY_USERNAME']
+  else:
+    print('Missing username')
+    sys.exit(-1)
+
+  if not args.password and 'NIXPLAY_PASSWORD' in os.environ:
+    args.password = os.environ['NIXPLAY_PASSWORD']
+  else:
+    print('Missing password')
+    sys.exit(-1)    
+
+  main(args)
+
+
